08018199f33e6c4e23e65edf9090c3018dc11e2b
Eloá Mello <eloamello126@gmail.com>
2022-11-29 14:51:41 -0300

    feat: gerador de media entre os valores dos SKUs e gerador de log

--- avaliaprecos.py
+++ avaliaprecos.py
@@ -1,10 +1,25 @@
+from datetime import datetime
 from time import sleep
+from openpyxl.styles import Font
 from openpyxl.workbook import Workbook
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
 class AvaliaPrecos:
+    @staticmethod
+    def media_entre_valores(skus):
+        media_precos = 0
+
+        for x in range(len(skus)):
+            refatorado = skus[x][1].replace('R$ ', '')
+            refatorado = refatorado.replace('.', '')
+            refatorado = refatorado.replace(',', '.')
+            media_precos += float(refatorado)
+
+        for x in range(len(skus)):
+            skus[x].append(media_precos)
+
     def acessa_loja(self, produto):
         driver = webdriver.Chrome()
         driver.get("https://www.americanas.com.br/")
@@ -14,10 +29,11 @@ class AvaliaPrecos:
                                        """//*[@id="rsyswpsdk"]/div/header/div[1]/div[1]/div/div[1]/form/button""")
         elemento.send_keys(produto)
         driver.execute_script("arguments[0].click()", pesquisa)
-        sleep(10)
+        sleep(5)
 
         skus = []
         i = 1
+
         while i <= 5:
             sku = []
             nome_produto = driver.find_element(By.XPATH,
@@ -28,15 +44,24 @@ class AvaliaPrecos:
             sku.append(valor_produto.text)
             skus.append(sku)
             i += 1
+
+        log = open("log.txt", 'a')
+        log.write(datetime.now().strftime('%d/%m/%y %H:%M:%S') + " - Pesquisado SKUs do produto {}\n".format(produto))
+
+        self.media_entre_valores(skus)
         return skus
 
-    def coloca_valores(self, produtos):
+    def gerador_relatorio_excel(self, produtos):
         workbook = Workbook()
         sheet = workbook.active
         sheet['A1'] = "Produtos"
         sheet['B1'] = "SKUs"
         sheet['C1'] = "Preços"
         sheet['D1'] = "Média dos Preços"
+        sheet['A1'].font = Font(bold=True)
+        sheet['B1'].font = Font(bold=True)
+        sheet['C1'].font = Font(bold=True)
+        sheet['D1'].font = Font(bold=True)
 
         row = 2
         for x in range(len(produtos)):
@@ -45,6 +70,10 @@ class AvaliaPrecos:
                 sheet["A{}".format(row)] = produtos[x]
                 sheet["B{}".format(row)] = resultado[y][0]
                 sheet["C{}".format(row)] = resultado[y][1]
+                sheet["D{}".format(row)] = resultado[y][2]
                 row += 1
 
-        workbook.save("relatorioProdutos.xlsx")
+        workbook.save("Relatório Produtos.xlsx")
+
+        log = open("log.txt", 'a')
+        log.write(datetime.now().strftime('%d/%m/%y %H:%M:%S') + " - Criado relatorio final\n")
--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from avaliaprecos import AvaliaPrecos
+import pandas
 
-produtos = ["iPhone 11 pro"]
 
-teste = AvaliaPrecos()
-teste.coloca_valores(produtos)
+def get_valores_excel(excel_sheet):
+    data_frame = (pandas.read_excel(excel_sheet)).values.tolist()
+    lista_produtos = []
+    for x in range(len(data_frame)):
+        lista_produtos.append(data_frame[x][0])
+    return lista_produtos
+
+
+excel = "GoLiveTech - Processo Seletivo - Desafio Técnico - Exemplo.xlsx"
+produtos = get_valores_excel(excel)
+avaliaprecos = AvaliaPrecos()
+avaliaprecos.gerador_relatorio_excel(produtos)
